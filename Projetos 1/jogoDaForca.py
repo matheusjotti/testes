3b99d887248b05d632fc2d1148654c64b604a20c
matheusjotti <116606397+matheusjotti@users.noreply.github.com>
2022-10-29 13:40:00 -0300

    Add files via upload

---
+++ Projetos 1/jogoDaForca.py
@@ -0,0 +1,23 @@
+'''
+Alunos:
+Matheus Zanella Jotti - RA: 1128541
+Alessandro da Silva
+'''
+
+import os
+os.system('cls')
+
+desafiante = input('Desafiante, digite seu nome: ')
+competidor = input('Competidor, digite seu nome: ')
+
+os.system('cls')
+
+print('Desafiante, insira as seguintes informações:')
+palavraChave = []
+palavraChave.append(input('Palavra chave:'))
+dica1 = input('Dica 1:')
+dica2 = input('Dica 2:')
+dica3 = input('Dica 3:')
+
+os.system('cls')
+
